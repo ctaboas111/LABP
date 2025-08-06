5893fa0cec05d4ee2183c76a256b7c100cb41453
ctaboas111 <cot3062@protonmail.com>
2025-08-05 22:22:54 -0300

    Add files via upload

---
+++ lista de exercícios 1/ex2a/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    int A, B, C;
+    printf("Hipotenusa! Lado A = ?\n");
+    printf("Cateto B: ");
+    scanf("%i", &B);
+
+    printf("Cateto C: ");
+    scanf("%i", &C);
+
+
+
+
+
+
+
+    return 0;
+}
